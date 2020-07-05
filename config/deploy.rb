c9a9ac455c98c9079fc8042a0859869e4e7ec616
KSK-engineer <mutekifield@yahoo.co.jp>
2020-07-05 16:20:45 +0900

    freeM-studying-deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "freeM"
-set :repo_url, "git@github.com:KSK-engineer/freeM.git"
+set :repo_url, "git@github.com/KSK-engineer/freeM.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
