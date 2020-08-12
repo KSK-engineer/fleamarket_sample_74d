afe7acd16089628c6c60b8a42f9b5fd646fb319e
httrhtm <tonton.voov888@docomo.ne.jp>
2020-08-12 12:54:00 +0900

    アソシエーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_one :address,     dependent: :destroy
+  has_one :credit_card, dependent: :destroy
+  has_many :items,      dependent: :destroy
+  has_many :likes,      dependent: :destroy
+  has_many :comments,   dependent: :destroy
 
   validates :nickname,           presence: true
 
