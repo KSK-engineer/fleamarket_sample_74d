e8d605640a1d5123b4445b0aa146502efc996c2d
httrhtm <tonton.voov888@docomo.ne.jp>
2020-08-12 12:27:23 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
+  validates :nickname,           presence: true
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,              presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :encrypted_password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+
+  validates :familyname_kanji,   presence: true, format: {with: /\A[^ -~｡-ﾟ]+\z/,}
+  validates :firstname_kanji,    presence: true, format: {with: /\A[^ -~｡-ﾟ]+\z/,}
+  validates :familyname_kana,    presence: true, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,} 
+  validates :firstname_kana,     presence: true, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,} 
+  validates :birthday,           presence: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_021154) do
+ActiveRecord::Schema.define(version: 2020_08_12_023200) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "postcode", null: false
+    t.integer "prefecture", null: false
+    t.string "city", null: false
+    t.string "block", null: false
+    t.string "building"
+    t.string "room_number"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -45,4 +59,5 @@ ActiveRecord::Schema.define(version: 2020_08_12_021154) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
 end
