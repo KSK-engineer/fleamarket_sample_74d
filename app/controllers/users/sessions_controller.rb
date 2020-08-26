98cf218f23f9eed7431ad40013ec84d2a07beb50
httrhtm <tonton.voov888@docomo.ne.jp>
2020-08-26 20:50:03 +0900

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,16 +4,16 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-  end
+  # def new
+  # end
 
   # POST /resource/sign_in
-  def create
-  end
+  # def create
+  # end
 
   # DELETE /resource/sign_out
-  def destroy
-  end
+  # def destroy
+  # end
 
   # protected
 
