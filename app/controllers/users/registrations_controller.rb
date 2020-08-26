a2db30fde3110a69bb940b4d0ad1975ae554f99e
httrhtm <tonton.voov888@docomo.ne.jp>
2020-08-26 20:50:00 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  def new
-  end
+  # def new
+  #   @user = User.new
+  # end
 
   # POST /resource
-  def create
-  end
+  # def create
+  #   @user = User.new(sign_up_params)
+  # end
 
   # GET /resource/edit
   # def edit
@@ -27,6 +29,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
+  # def after_sign_up_path_for(resource)
+  #   root_path
+  # end
+
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
@@ -40,7 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
