e6c5ccc8e30a2750a1bb058f483d498e6f6b6869
httrhtm <tonton.voov888@docomo.ne.jp>
2020-08-28 12:29:25 +0900

    Update registrations_controller.rb
    新規登録・ログインを有効にするための記述

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,18 +1,21 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   @user = User.new
-  # end
+  def new
+    @user = User.new
+  end
 
   # POST /resource
-  # def create
-  #   @user = User.new(sign_up_params)
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    redirect_to root_path
+  end
 
   # GET /resource/edit
   # def edit
@@ -29,10 +32,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # def after_sign_up_path_for(resource)
-  #   root_path
-  # end
-
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
   # in to be expired now. This is useful if the user wants to
@@ -42,12 +41,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
