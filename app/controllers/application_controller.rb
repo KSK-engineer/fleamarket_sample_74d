4ee7f730a42c7b962c926d418d22160db32be839
httrhtm <tonton.voov888@docomo.ne.jp>
2020-08-28 12:28:42 +0900

    キーを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birthday])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :familyname_kanji, :firstname_kanji, :familyname_kana, :firstname_kana, :birthday])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :familyname_kanji, :firstname_kanji, :familyname_kan, :firstname_kana, :birthday])
   end
 
   private
