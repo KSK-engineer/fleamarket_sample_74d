9e80590cd6a7ba35576f7b4aa96b5587fe52b8c5
akinori0518 <aki.yamamoto0518@gmail.com>
2020-08-31 19:00:15 +0900

    cards.rb

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2020_08_12_023200) do
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
