919cd61ca44a237a544a45b35e79a6c1d12c9f84
ryoichi1006 <kubota.r.10006@gmail.com>
2020-09-04 13:08:04 +0900

    pathをprefixに変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @image = @item.images.first
     @images = @item.images
-    @address = Prefecture.find_by(id: @item.prefecture_id)
-    @seller = User.find_by(id: @item.seller_id)
+    @address = Prefecture.find(@item.prefecture_id)
+    @seller = User.find(@item.seller_id)
   end
 end
